0e6bdd754a649f62dd1e07b3948195ced6498c17
Scravlon <haokokyong10@gmail.com>
2019-08-03 22:57:58 -0400

    Initial commit

---
+++ app/src/main/java/com/scravlon/opencvjava/MainActivity.java
@@ -0,0 +1,18 @@
+package com.scravlon.opencvjava;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color_blob);
+        Intent intent = new Intent(MainActivity.this,FdActivity.class);
+        startActivity(intent);
+    }
+}
